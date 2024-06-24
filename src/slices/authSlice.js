b50210b8a03d2a71fa5cca16f435e58c5641338e
sun-gwang <kkj80911@gmail.com>
2024-06-24 16:42:06 +0900

    로그인

--- src/slices/authSlice.js
+++ src/slices/authSlice.js
@@ -1,22 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { getCookie, setCookie, removeCookie } from "../../src/util/cookieUtil";
+import { getCookie, setCookie, removeCookie } from "../utils/cookieUtil";
 
 const loadStateFromCookie = () => {
   const auth = getCookie("auth");
 
-  const uid = auth?.uid;
+  const email = auth?.email;
   const accessToken = auth?.accessToken;
+  const refreshToken = auth?.refreshToken;
+  const role = auth?.role;
 
   // 리턴에 값을 넣어줘야 초기화가 된 후에도 값이 유지가 됨 //
   return {
-    uid,
+    email,
     accessToken,
+    refreshToken,
+    role,
   };
 };
 
 const initState = {
-  uid: "",
+  email: "",
   accessToken: "",
+  refreshToken: "",
+  role: "",
 };
 
 const authSlice = createSlice({
@@ -29,8 +35,10 @@ const authSlice = createSlice({
       const data = action.payload;
 
       // 리덕스 저장소 상태 업데이터(Username과 accessToken)
-      state.uid = data.uid;
+      state.email = data.email;
       state.accessToken = data.accessToken;
+      state.refreshToken = data.refreshToken;
+      state.role = data.role;
 
       // 쿠키저장 (쿠키이름, 값, 유효기간)
       setCookie("auth", data, 1);
@@ -44,8 +52,10 @@ const authSlice = createSlice({
       // 프로필 정보 업데이트 액션
       const updateData = action.payload;
 
-      state.uid = updateData.uid;
+      state.email = updateData.email;
       state.accessToken = updateData.accessToken;
+      state.refreshToken = updateData.refreshToken;
+      state.role = updateData.role;
 
       // 쿠키저장
       setCookie("auth", updateData, 1);
