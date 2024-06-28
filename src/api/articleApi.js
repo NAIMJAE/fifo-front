6a5df2bb4b6f7c7a43a058cbd4902c70bad05891
NAIMJAE <skdlawo5985@gamil.com>
2024-06-29 02:26:40 +0900

    댓글 작성중

--- src/api/articleApi.js
+++ src/api/articleApi.js
@@ -20,6 +20,12 @@ export const articleListApi = async (data) => {
 
 /** 게시글 보기 API */
 export const articleViewApi = async (pno) => {
-    const response = await axios.get(`${rootURL}/post`, pno);
+    const response = await axios.get(`${rootURL}/post/${pno}`);
+    return response.data;
+};
+
+/** 댓글 작성 API */
+export const commentInsertApi = async (comment) => {
+    const response = await axios.post(`${rootURL}/comment`, comment);
     return response.data;
 };
--- src/components/article/CommentWriteComponent.js
+++ src/components/article/CommentWriteComponent.js
@@ -1,16 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const CommentWriteComponent = () => {
+const CommentWriteComponent = ({ commentNum, insertComment, loginSlice }) => {
+
+  const [comment, setComment] = useState({
+    content: "",
+    userNo: "",
+    userName: "",
+    pno: "",
+  });
+
+  /** 댓글 입력 textarea 관리 */
+  const handleChange = (e) => {
+    setComment(prev => ({...prev, content: e.target.value}));
+  };
+
+  /** 댓글 작성 버튼 */
+  const handleSubmit = () => {
+    if (comment.content.trim() !== "") {
+      let result = window.confirm("댓글을 작성하시겠습니까?");
+      if (result ) {
+        insertComment(comment);
+        setComment({ content: "" });
+      }
+    }else {
+      alert("댓글을 입력해주세요.");
+    }
+  };
+
   return (
     <>
-        <p>댓글 265 개</p>
+        <p>댓글 {commentNum} 개</p>
         <div>
             <img src="../../images/ppoppi.png" alt="profile" />
             <p>우리집뽀삐</p>
         </div>
         <div>
-            <textarea name="" id=""></textarea>
-            <button>작성</button>
+            <textarea value={comment.content} onChange={handleChange}></textarea>
+            <button onClick={handleSubmit}>작성</button>
         </div>
     </>
   )
