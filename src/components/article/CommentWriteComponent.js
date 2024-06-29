b80e8221e88a9e57251263a9b88246fd368264d9
NAIMJAE <skdlawo5985@gamil.com>
2024-06-30 01:50:03 +0900

    게시글 댓글 작성 수정 추가

--- src/components/article/CommentWriteComponent.js
+++ src/components/article/CommentWriteComponent.js
@@ -1,41 +1,53 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
-const CommentWriteComponent = ({ commentNum, insertComment, loginSlice }) => {
+const CommentWriteComponent = ({ comNum, insertComment, loginSlice }) => {
 
+  const textareaRef = useRef(null);
+
+  /** 작성할 댓글 정보 저장 */
   const [comment, setComment] = useState({
     content: "",
-    userNo: "",
-    userName: "",
-    pno: "",
+    userNo: 1, 
+    pno: 0,
   });
 
   /** 댓글 입력 textarea 관리 */
   const handleChange = (e) => {
-    setComment(prev => ({...prev, content: e.target.value}));
+    setComment(prev => ({ ...prev, content: e.target.value }));
   };
 
   /** 댓글 작성 버튼 */
   const handleSubmit = () => {
-    if (comment.content.trim() !== "") {
+    if (comment.content.trim() !== "" && comment.content.length < 1000) {
       let result = window.confirm("댓글을 작성하시겠습니까?");
-      if (result ) {
+      if (result) {
         insertComment(comment);
-        setComment({ content: "" });
+        setComment({ ...comment, content: "" });
       }
-    }else {
+    } else if (comment.content.length > 1000) {
+      alert("댓글은 최대 1000자까지 작성가능합니다.");
+    } else {
       alert("댓글을 입력해주세요.");
     }
   };
 
+  /** textarea 높이 자동 조절 */
+  useEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = 'auto';
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+    }
+  }, [comment]);
+
   return (
     <>
-        <p>댓글 {commentNum} 개</p>
+        <p>댓글 {comNum} 개</p>
         <div>
             <img src="../../images/ppoppi.png" alt="profile" />
             <p>우리집뽀삐</p>
         </div>
         <div>
-            <textarea value={comment.content} onChange={handleChange}></textarea>
+            <textarea ref={textareaRef} value={comment.content} onChange={handleChange}></textarea>
             <button onClick={handleSubmit}>작성</button>
         </div>
     </>
