7f26fa82b84683657f5309e4d7f0c545d1e54744
unknown <y01044285848@gmail.com>
2024-07-09 11:20:56 +0900

    컴파일 기능 추가

--- src/pages/grade/QuestionViewPage.js
+++ src/pages/grade/QuestionViewPage.js
@@ -2,27 +2,58 @@ import React, { useEffect, useRef, useState } from 'react'
 import MainLayout from '../../layout/MainLayout'
 import '../../styles/grade.scss'
 import '../../components/grade/LanguageButtonComponent'
-import QuestionContentsComponent from '../../components/grade/QuestionContentsComponent'
+import MonacoEditor from 'react-monaco-editor';
 import axios from 'axios'
 
 const QuestionViewPage = () => {
+
+    const questionNo = useRef(parseInt(new URLSearchParams(window.location.search).get("no")));
+    const [language, setLanguage] = useState('');
+    const [code, setCode] = useState('');
+    const [questionInfo, setQuestionInfo] = useState({});
+    const [output, setOutput] = useState('??');
     
-    const questionNo = useRef(new URLSearchParams(window.location.search).get("no"));
+    const handleExecute = async () => {
+        
+      const response = await fetch('http://localhost:8080/fifo-back/question/execute', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ 
+            questionNo: questionInfo.current, 
+            language: language, 
+            code: code
+        })
+      });
+      const result = await response.json();
+      setOutput(result.output);
+      console.log(result)
+    };
 
     /** 선택 언어 문제 리스트 조회 */
     useEffect(() => {
         axios.get(`http://localhost:8080/fifo-back/question/${questionNo.current}`)
-        .then((res) => {
-            console.log(res.data)
-            
-        })
+            .then((res) => {
+                console.log(res.data)
+                setQuestionInfo(res.data)
+                setLanguage('java')
+            })
     }, [])
 
     return (
         <MainLayout>
-            
-            <div id='questionView'>
 
+            <div id='questionView'>
+                <MonacoEditor
+                    height="400"
+                    language={language}
+                    theme="vs-dark"
+                    value={code}
+                    onChange={(newValue) => setCode(newValue)}
+                />
+                <button onClick={handleExecute}>Execute</button>
+                <div>
+                    {output}
+                </div>
             </div>
         </MainLayout>
     )
