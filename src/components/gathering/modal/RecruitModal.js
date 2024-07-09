694daee5b3009672a38edb64e0ca2be8072bd822
NAIMJAE <skdlawo5985@gmail.com>
2024-07-09 17:27:30 +0900

    참여현황모달

--- src/components/gathering/modal/RecruitModal.js
+++ src/components/gathering/modal/RecruitModal.js
@@ -1,28 +1,46 @@
 import React, { useState } from 'react'
+import { info } from 'sass';
 
 const RecruitModal = ({handleModal}) => {
 
-    const [recruitInfo, setRecruitInfo] = useState(false);
+    /** 모모달 띄우는 상태 */ 
+    const [popState, setPopState] = useState(false);
 
+    /** 모모달에 출력할 정보 */ 
+    const [recruit, setRecruit] = useState("");
+    
+    /** 모달에 출력할 정보 */ 
     const [example, setExample] = useState([
         {
-            name : "김선광",
-            state : "수락대기"
+            id: 0,
+            name: "김선광",
+            state: "수락대기",
+            pop: false,
+            on: 24,
+            // 매너온도, 활동지역, 언어레벨?
         },
         {
-            name : "박선광",
-            state : "수락완료"
+            id: 1,
+            name: "잇응윤",
+            state: "수락대기",
+            pop: false,
+            on: 34,
         },
         {
-            name : "이선광",
-            state : "수락거절"
-        }
+            id: 2,
+            name: "바김재",
+            state: "수락대기",
+            pop: false,
+            on: 100,
+        },
     ]);
 
-    const [recruit, setRecruit] = useState("");
-
-    const overMouse = () => {
-     // 마우스 오버에서 값을 넘겨주기에는 성능이 안나올듯 생각해보자   
+    /** 모달에서 모모달 띄우는 함수 */
+    // example에 있는 유저 상세 정보 넣기
+    const overMouse = (index) => {
+        const foundData = example.find(info => info.id === index);
+        setRecruit(foundData);
+        setPopState(true);
     }
 
   return (
@@ -30,22 +48,22 @@ const RecruitModal = ({handleModal}) => {
         <div className='recruitModal'>
             {example && example.map((each, index) => (
                 <div className='cntRow' key={index} style={{justifyContent:"space-between"}} 
-                    onMouseOver={() => setRecruitInfo(true)}
-                    onMouseLeave={() => setRecruitInfo(false)}>
+                    onMouseOver={() => overMouse(index)}>
                     <p>{each.name}</p>
                     <h3>{each.state}</h3>
                 </div>
             ))}
-            
         </div>
-        {recruitInfo && 
+        {(popState && recruit) &&
             <div className='recruitInfoModal'>
                 <div className='cntRow' style={{justifyContent:"space-between"}}>
-                    <p>김선광</p>
-                    <h3>수락대기</h3>
+                    <p>{recruit.name}</p>
+                    <h3>{recruit.state}</h3>
+                    <h3>{recruit.on}도</h3>
                 </div>
             </div>
         }
+            
     </div>
   )
 }
