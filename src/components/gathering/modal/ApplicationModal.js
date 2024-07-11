5320e64227c9b8563a45eafdf65f3ac5227c0507
NAIMJAE <skdlawo5985@gmail.com>
2024-07-11 14:52:18 +0900

    참여신청모달중

---
+++ src/components/gathering/modal/ApplicationModal.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ApplicationModal = ({ handleModal }) => {
+  return (
+    <div className='recruitModalBox' onClick={handleModal}>
+        ㅎ2    
+    </div>
+  )
+}
+
+export default ApplicationModal
