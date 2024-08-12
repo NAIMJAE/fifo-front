ed840c11e0608788e95a864233460bff42e1cf68
sun-gwang <142211574+sun-gwang@users.noreply.github.com>
2024-08-13 06:58:45 +0900

    문서 끄적, 스킬 삭제

--- src/components/mooim/Document.js
+++ src/components/mooim/Document.js
@@ -3,12 +3,20 @@ import { useCreateBlockNote } from "@blocknote/react";
 import { BlockNoteView } from "@blocknote/mantine";
 import * as Y from "yjs";
 import { WebrtcProvider } from "y-webrtc";
-import { uploadFile, getDocumentSection } from "../../api/documentApi";
+import {
+  uploadFile,
+  getDocumentSection,
+  deleteDocument,
+} from "../../api/documentApi";
 import "@blocknote/mantine/style.css";
 import { RootUrl } from "../../api/RootUrl";
 import "../../styles/document.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
+import {
+  faEllipsis,
+  faEllipsisV,
+  faPlusCircle,
+} from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
 
 const Document = ({ mooimno }) => {
@@ -38,28 +46,48 @@ const Document = ({ mooimno }) => {
 
   /**소켓 연결, db의 에디터 정보 가져오기 */
   const [docno, setDocno] = useState(""); // 보여줄 에디터의 PK값
+
+  // useEffect(() => {
+  //   console.log(editor.document);
+  //   console.log(JSON.stringify(editor.document));
+  // }, [editor.document]);
+
   useEffect(() => {
     /**소켓 연결*/
     provider.current = new WebrtcProvider(mooimno, doc, {
       signaling: [`${RootUrl()}/document/${mooimno}`],
     });
 
-    /**현재 존재하는 문서들 가져오기 */
-    selectDocument();
+    // 문서를 가져오는 함수 정의
+    const fetchDocument = async () => {
+      try {
+        // 현재 존재하는 문서들 가져오기
+        await selectDocument();
 
-    /**선택한 문서의 내용 가져오기 */
-    console.log(docno);
-    try {
-      const response = axios.get(`${RootUrl()}/document/getDocument?docno=9`);
-      const data = response.data.content;
-      console.log(data);
-      const docView = JSON.parse(data.content);
+        // 문서가 설정되어 있을 때만 호출
+        if (docno) {
+          console.log("docno설정" + docno);
+          const response = await axios.get(
+            `${RootUrl()}/document/getDocument?docno=${docno}`
+          );
+          const data = response.data;
+          console.log(data.content);
 
-      // 에디터에 노트 데이터 적용
-      editor.replaceBlocks(data.content, docView);
-    } catch (err) {
-      console.error(err);
-    }
+          // 에디터에 content 불러오기
+          editor.replaceBlocks(
+            "ac30a7f5-519e-4fa8-9985-7442008b592d",
+            data.content
+          );
+        } else {
+          console.warn("docno가 설정되지 않았습니다.");
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    // 비동기 함수 호출
+    fetchDocument();
 
     // 컴포넌트가 언마운트될 때 provider를 정리.
     return () => {
@@ -79,7 +107,9 @@ const Document = ({ mooimno }) => {
         }))
       );
       // 맨 위 문서를 보이게
-      setDocno(documentList[0].docno);
+      if (!docno) {
+        setDocno(documentList[0].docno);
+      }
     } catch (err) {
       console.log(err);
     }
@@ -113,11 +143,27 @@ const Document = ({ mooimno }) => {
     }
   };
 
+  /**문서를 삭제 할 수 있는 버튼 띄우기 */
+  const [activeDelete, setActiveDelete] = useState(null);
   const handleSectionClick = (id) => {
     setDocno(id);
+    setActiveDelete(id);
   };
 
-  useEffect(() => {}, [docno]);
+  /**삭제 버튼 */
+  const handleDelete = async (id) => {
+    if (window.confirm("삭제하시겠습니까?")) {
+      const response = await deleteDocument(id);
+      console.log(response);
+
+      if (response === 1) {
+        setDocno("");
+        alert("삭제되었습니다.");
+      } else {
+        alert("삭제에 실패했습니다.");
+      }
+    }
+  };
 
   return (
     <div className="Document">
@@ -127,12 +173,19 @@ const Document = ({ mooimno }) => {
           <FontAwesomeIcon icon={faPlusCircle} size="lg" color="#7b7b7b" />
         </span>
         {sections.map((section) => (
-          <section
-            key={section.id}
-            onClick={() => handleSectionClick(section.id)}
-          >
-            {section.title}
-          </section>
+          <div key={section.id}>
+            <section
+              id={section.id}
+              onClick={() => handleSectionClick(section.id)}
+              style={{ cursor: "pointer" }}
+            >
+              {section.title}
+            </section>
+
+            {activeDelete === section.id && (
+              <button onClick={() => handleDelete(section.id)}>삭제</button>
+            )}
+          </div>
         ))}
       </div>
       <BlockNoteView
